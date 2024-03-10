3028e3c4672df17fb885c750e74b596308c45d71
Craig Thacker <craigthackerx@gmail.com>
2024-03-10 00:06:06 +0000

    Update code

--- variables.tf
+++ variables.tf
@@ -1,6 +1,13 @@
 variable "cidrs" {
   type = object({
-    management = optional(object({
+    mgmt = optional(object({
+      prd = optional(list(string))
+      ppd = optional(list(string))
+      uat = optional(list(string))
+      tst = optional(list(string))
+      dev = optional(list(string))
+    }))
+    lbd = optional(object({
       prd = optional(list(string))
       ppd = optional(list(string))
       uat = optional(list(string))
@@ -10,12 +17,19 @@ variable "cidrs" {
   })
   description = "A variable which helps store CIDRs"
   default = {
-    management = {
+    mgmt = {
       prd = ["10.0.0.0/22"]
       ppd = ["10.4.0.0/22"]
       uat = ["10.8.0.0/22"]
       tst = ["10.12.0.0/22"]
       dev = ["10.16.0.0/22"]
+    }
+    lbd = {
+      prd = ["10.20.0.0/22"]
+      ppd = ["10.24.0.0/22"]
+      uat = ["10.28.0.0/22"]
+      tst = ["10.32.0.0/22"]
+      dev = ["10.36.0.0/22"]
     }
   }
 }
